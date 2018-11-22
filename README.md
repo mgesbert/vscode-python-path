92e42040293c354e7e4a2ad80019be8c4e8badf5
Mathias Gesbert <mathias.gesbert@gmail.com>
2018-11-22 13:55:41 +0100

    Command "Copy python path"

---
+++ README.md
@@ -0,0 +1,13 @@
+# Python Path
+
+## Features:
+
+- [x] Copy python path of current file from command
+- [ ] Copy python path of current file from contextual menu
+- [ ] Copy python path of a file from explorer
+- [ ] Generate import line for a class
+- [ ] Generate import line for a module
+
+## Miscellaneous
+
+Inspiration from the Sublime Package: https://github.com/Mimino666/SublimeText2-python-package-to-clipboard
---
+++ extension.js
@@ -0,0 +1,53 @@
+const vscode = require("vscode");
+const fs = require("fs");
+const clipboardy = require("clipboardy");
+
+function getPythonPath(path) {
+  const splittedPath = path.split("/");
+  if (
+    splittedPath.length === 0 ||
+    !splittedPath[splittedPath.length - 1].endsWith(".py")
+  ) {
+    return "";
+  }
+
+  const fileName = splittedPath.pop();
+
+  // removing extension
+  let pythonPath =
+    fileName !== "__init__.py"
+      ? [fileName.substring(0, fileName.lastIndexOf("."))]
+      : [];
+
+  while (
+    splittedPath.length > 0 &&
+    fs.existsSync([...splittedPath, ["__init__.py"]].join("/"))
+  ) {
+    pythonPath.unshift(splittedPath.pop());
+  }
+
+  return pythonPath.join(".");
+}
+
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "extension.copyPythonPath",
+    function() {
+      try {
+        const pythonPath = getPythonPath(
+          vscode.window.activeTextEditor.document.fileName
+        );
+        if (pythonPath) {
+          clipboardy.writeSync(pythonPath);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+function deactivate() {}
+exports.deactivate = deactivate;
